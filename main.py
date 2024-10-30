f005ab431ef7622616718cdd6d2684b1aa9912e7
Vlad Cebotari <vladcebotari789@gmail.com>
2024-10-30 14:48:18 +0200

    Modified main for redeploy

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ app.include_router(account.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+
+    allow_origins=["http://localhost:3000","http://localhost:8000"],
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
